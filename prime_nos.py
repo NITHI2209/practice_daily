602b0e69a1081058df7fb89069ea2085a14a85a3
NITHI2209 <nithyasree065@gmail.com>
2025-07-08 19:22:58 +0530

    Update prime_nos.py

--- prime_nos.py
+++ prime_nos.py
@@ -1,1 +1,14 @@
-
+#08/07/2025 
+n = int(input("enter the number: "))
+flag = 0
+if n <= 1:
+    print("its not a prime number")
+else:
+    for i in range(2,n):
+        if n%i == 0:
+            flag = 1 
+            break
+if flag == 1:
+    print(f"{n} is not a prime")
+else:
+    print(f"{n} is a prime nos")
