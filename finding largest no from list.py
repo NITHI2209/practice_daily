7f0133ca9ab66b9c3c5a80266fa3bd6a127188a0
NITHI2209 <nithyasree065@gmail.com>
2025-07-11 00:19:55 +0530

    Create finding largest no from list.py

---
+++ finding largest no from list.py
@@ -0,0 +1,9 @@
+#10/07/2025
+num = [10, 90, 88, 87, 96, 54]
+largest = num[0]  # Start by assuming the first number is the largest
+
+for i in num:
+    if i > largest:
+        largest = i
+
+print("Largest number is:", largest)
