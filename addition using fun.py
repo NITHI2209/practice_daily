606007a6415c9fed128b9cfb143c7547c05c52a1
NITHI2209 <nithyasree065@gmail.com>
2025-07-14 19:08:41 +0530

    Create addition using fun.py

---
+++ addition using fun.py
@@ -0,0 +1,14 @@
+#14/07/2025
+#question:
+#Add two nos using function
+#code:
+def addition_numbers(a,b):
+    return a+b 
+num1 = int(input("enter the number: "))
+num2 = int(input("enter the number: "))
+result = addition_numbers(num1,num2)
+print("The sum of nos:",result)
+#sample:
+# enter the number: 77
+# enter the number: 54
+# The sum of nos: 131
