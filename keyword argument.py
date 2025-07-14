b91d38dc1cdfc994f4758b36756c6e096b07db88
NITHI2209 <nithyasree065@gmail.com>
2025-07-14 20:07:38 +0530

    Create keyword argument.py

---
+++ keyword argument.py
@@ -0,0 +1,15 @@
+#14/07/2025
+#Write a function called employee_details(name, department) that:
+# Prints both name and department
+# Then call the function using keyword arguments in any order
+#code:
+def employee_info(name,age,department):
+    print("Name:",name)
+    print("Age:",age)
+    print("Department:",department)
+
+employee_info(department="HR",name="Nithya",age=20) 
+#sample:
+# Name: Nithya
+# Age: 20
+# Department: HR
