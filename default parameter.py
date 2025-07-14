1fc5e88a7d01df54354386d1674575afc4153709
NITHI2209 <nithyasree065@gmail.com>
2025-07-14 19:34:49 +0530

    Create default parameter.py

---
+++ default parameter.py
@@ -0,0 +1,12 @@
+#14/07/2025
+#question
+#default parameter 
+#1-Call without passing a name
+#2-Call with your name
+#code:
+def greet(name="friend"):
+    print("hello welcome",name)
+#1-
+greet() 
+#2-
+greet("nithya")
