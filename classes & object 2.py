27aef0727d9d14e8b6fda4cbe596df5e0194a986
NITHI2209 <nithyasree065@gmail.com>
2025-07-16 23:25:26 +0530

    Create classes & object 2.py

---
+++ classes & object 2.py
@@ -0,0 +1,20 @@
+#16/07/2025
+#learning concepts:
+class laptop():
+    price = 0 
+    processor =" "
+    ram =" "
+    
+hp = laptop()
+dell = laptop()
+ 
+hp.price = 50000
+hp.processor = "i5"
+hp.ram ="8Gb"
+
+dell.price = 90000
+dell.processor ="i7"
+dell.ram ="16Gb"
+
+print(hp.price)
+print(dell.price)
