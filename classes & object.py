2735a288a61320aab785a99a8e054daf0727283e
NITHI2209 <nithyasree065@gmail.com>
2025-07-16 21:11:55 +0530

    Create classes & object.py

---
+++ classes & object.py
@@ -0,0 +1,23 @@
+#16/07/2025
+#learning concept using examples
+class goa:
+    name=""
+    drink = ""
+    def beach(self):
+        print("lets go beach")
+    def party(self):
+        print("lets go party")
+
+ramesh = goa()
+suresh = goa() 
+
+ramesh.name = "RAMESH"
+ramesh.drink = "yes"
+suresh.name = "SURESH"
+suresh.drink ="no"
+print(ramesh.name)
+print("drink:",ramesh.drink)
+print(ramesh.name)
+print("drink:",ramesh.drink)
+ramesh.party()
+suresh.beach()
