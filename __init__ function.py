4f89fc087b07cf23012c0b860a2c68563d07a566
NITHI2209 <nithyasree065@gmail.com>
2025-07-16 23:43:49 +0530

    Create __init__ function.py

---
+++ __init__ function.py
@@ -0,0 +1,14 @@
+#16/07/2025
+#code:
+class laptop:
+    def __init__(self):
+        print("demo")
+    def display(self):
+        print("display")
+
+hp = laptop()
+
+
+#ouput: demo
+# __init__ is a special method in Python classes.
+# It is called automatically when a new object is created.
